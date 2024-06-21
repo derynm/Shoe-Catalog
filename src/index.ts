b67abf24f5e7c0682fda8b30409dbf2fd650a3e7
derynm <dery02.nm@gmail.com>
2024-06-21 19:21:12 +0700

    feat: add data type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { shoesData } from './data/shoes'
 
+
+let shoes = shoesData
 const app = new Hono()
 
 app.get('/', (c) => {
@@ -10,18 +12,40 @@ app.get('/', (c) => {
 })
 
 app.get("/shoes", (c) => {
-  return c.json(shoesData);
+  return c.json(shoes);
 });
 
 app.get("/shoes/:id", (c) => {
   const id = Number(c.req.param('id'));
-  const shoe = shoesData.find((s) => s.id === id);
+  const shoe = shoes.find((s) => s.id === id);
 
   if (!shoe) {
     return c.json({ message: "Shoe not found" }, 404);
   }
 
   return c.json(shoe);
+});
+
+
+app.delete("/shoes/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  if (!id) {
+    return c.json({ message: "Shoe not found" });
+  }
+
+  const shoeId = shoes.map((shoe)=> shoe.id);
+
+  if (!shoeId.includes(id)){
+    return c.json({ message: "shoe to be deleted not found" });
+  }
+
+  const deletedShoe = shoes.find((shoe) => shoe.id === id);
+  shoes = shoes.filter((shoe) => shoe.id !== id);
+
+  return c.json({
+    message: `${deletedShoe?.brand} ${deletedShoe?.model} has been deleted successfully`,
+  });
 });
 
 export default app
