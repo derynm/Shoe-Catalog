e91c7e1ac73cb08f5feba85c6d18958e68e9445d
derynm <dery02.nm@gmail.com>
2024-06-30 11:14:17 +0700

    feat: add new API get shoe by id

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,36 @@ app.get('/', (c) => {
 })
 
 app.get("/shoes", async (c) => {
-  const shoes = await prisma.shoe.findMany();
+  const shoes = await prisma.shoe.findMany({
+    include:{
+      brand: true,
+      category: true,
+      Stock: true
+    }
+  });
   return c.json(shoes);
 });
+
+app.get("/shoes/:id", async (c) => {
+  const id = Number(c.req.param('id'));
+  const shoe = await prisma.shoe.findUnique({
+    where:{
+      id
+    },
+    include:{
+      brand: true,
+      category: true,
+      Stock: true
+    }
+  });
+
+  if (!shoe) {
+    return c.json({ message: "Shoe not found" }, 404);
+  }
+
+  return c.json(shoe);
+})
+
 
 // app.get("/shoes/:id", (c) => {
 //   const id = Number(c.req.param('id'));
